f022f4b6eb8173c08a005f31004c0d98e8ca76e8
woolfrey <jonathan.woolfrey@gmail.com>
2020-04-01 15:25:04 +1100

    Working on constructor

--- include/MobileManipulator.h
+++ include/MobileManipulator.h
@@ -1,4 +1,6 @@
+#include "Eigen/Dense"						// Required for matrix inversion
 #include "geometry_msgs/Accel.h"
+#include "mobile_manipulator/Vehicle.h"
 #include "SerialLink.h"
 
 class MobileManipulator
@@ -6,8 +8,8 @@ class MobileManipulator
 	public:
 		// Properties
 		geometry_msgs::Pose baseTF;			// Transform from vehicle origin to manipulator base
-		//mobile_manipulator::Vehicle vehicle;		// Vehicle object
-		//serial_link::Serial arm;			// Serial-Link object
+		mobile_manipulator::Vehicle vehicle;		// Vehicle object
+		SerialLink arm;					// Serial-Link object
 
 		// Constructor(s)
 		MobileManipulator();
@@ -19,7 +21,7 @@ class MobileManipulator
 
 
 		// Set Functions
-		void updateState(const geometry_msgs::Pose &pose,
+		void updateState(const geometry_msgs::PoseStamped &input,
 				 const geometry_msgs::Twist &vel,
 				 const geometry_msgs::Accel &accel,
 				 const sensor_msgs::JointState &jointState);
@@ -27,6 +29,11 @@ class MobileManipulator
 	private:
 
 		// Properties
+		Eigen::MatrixXd A;				// B*T
+		Eigen::Matrix<double,6,6> B;			// Base velocity mapping to end-effector
+		Eigen::MatrixXd T;				// Thruster conversion matrix
+
+		
 
 		// Get Functions
 
@@ -45,23 +52,86 @@ MobileManipulator::MobileManipulator()
 
 MobileManipulator::MobileManipulator(int Hz)
 {
+	const int n = this->vehicle.thruster.size();		// No. of thrusters in this model
 
+
+	this->A.resize(6,n);
+	this->B.setIdentity();
+	this->T.resize(6,n);
+	
+
+	// Compute thruster transform matrix
+	for(int i = 0; i < n; i++)
+	{
+		T(0,i) = vehicle.thruster[i].axis.x;
+		T(1,i) = vehicle.thruster[i].axis.y;
+		T(2,i) = vehicle.thruster[i].axis.z;
+		
+		T(3,i) = vehicle.thruster[i].axis.y*vehicle.thruster[i].position.z 
+		       - vehicle.thruster[i].axis.z*vehicle.thruster[i].position.y;
+
+		T(4,i) = vehicle.thruster[i].axis.z*vehicle.thruster[i].position.x
+		       - vehicle.thruster[i].axis.x*vehicle.thruster[i].position.z;
+
+		T(5,i) = vehicle.thruster[i].axis.x*vehicle.thruster[i].position.y
+		       - vehicle.thruster[i].axis.y*vehicle.thruster[i].position.x;
+	}
 }
 
-void MobileManipulator::updateState(const geometry_msgs::Pose &pose,
+void MobileManipulator::updateState(const geometry_msgs::PoseStamped &input,
 				    const geometry_msgs::Twist &vel,
 				    const geometry_msgs::Accel &accel,
 				    const sensor_msgs::JointState &jointState)
 {
-	//this->vehicle.pose = pose;
-	//this->vehicle.velocity = vel;
-	//this->vehicle.acceleration = accel;
-	//this->arm.updateState(jointState,multiplyPose(pose.pose,this->baseTF);
+	this->vehicle.tf = input;
+	this->vehicle.vel = vel;
+	this->vehicle.accel = accel;
+	this->arm.updateState(jointState,multiplyPose(input.pose,this->baseTF));
+
+	// Update skew-symmetric component of base velocity mapping
+	double x = this->arm.FK.poses[this->arm.n].position.x - this->vehicle.tf.pose.position.x;
+	double y = this->arm.FK.poses[this->arm.n].position.y - this->vehicle.tf.pose.position.y;
+	double z = this->arm.FK.poses[this->arm.n].position.z - this->vehicle.tf.pose.position.z;
+	this->B(0,4) = z;
+	this->B(0,5) = -y;
+	this->B(1,3) = -z;
+	this->B(1,5) = x;
+	this->B(2,3) = y;
+	this->B(2,4) = -x;
+	
 }
 
-sensor_msgs::JointState MobileManipulator::rmrc(const geometry_msgs::Pose &pos, const geometry_msgs::Twist &vel)
+sensor_msgs::JointState MobileManipulator::rmrc(const geometry_msgs::Pose &pos,
+						const geometry_msgs::Twist &vel)
 {
 
+	
+/*geometry_msgs::Twist relative;					// Relative velocity to be passed to the manipulator
+
+	// End-effector position - vehicle position
+	r.x = this->arm.FK[this->arm.n].position.x - this->vehicle.tf.pose.position.x;
+	r.y = this->arm.FK[this->arm.n].position.y - this->vehicle.tf.pose.position.y;
+	r.z = this->arm.FK[this->arm.n].position.z - this->vehicle.tf.pose.position.z;
+
+
+	// Relative linear velocity
+	relative.linear.x = vel.linear.x - this->vehicle.vel.linear.x
+					 + this->vehicle.vel.angluar.z*r.y
+					 - this->vehicle.vel.angular.y*r.x;
+
+	relative.linear.y = vel.linear.y - this->vehicle.vel.linear.y
+					 + this->vehicle.vel.angular.x*r.z
+					 - this->vehicle.vel.angular.z*r.x;
+
+	relative.linear.z = vel.linear.z - this->vehicle.vel.linear.z
+					 + this->vehicle.vel.angular.y*r.x
+					 - this->vehicle.vel.angular.x*r.y;
+
+	// Relative angular velocity
+	relative.angular.x = vel.angular.x - this->vehicle.vel.angular.x;
+	relative.angular.y = vel.angular.y - this->vehicle.vel.angular.y;
+	relative.angular.z = vel.angular.z - this->vehicle.vel.angular.z;
+*/
 }
 
 
