808c6494e626f1d6565ae05d8c405b0de4abb347
woolfrey <jonathan.woolfrey@gmail.com>
2020-04-02 15:30:03 +1100

    Completed end-effector control function, added custom control message containing joint & thruster commands.

--- include/MobileManipulator.h
+++ include/MobileManipulator.h
@@ -1,5 +1,6 @@
 #include "Eigen/Dense"						// Required for matrix inversion
 #include "geometry_msgs/Accel.h"
+#include "mobile_manipulator/Control.h"
 #include "mobile_manipulator/Vehicle.h"
 #include "SerialLink.h"
 
@@ -8,7 +9,9 @@ class MobileManipulator
 	public:
 		// Properties
 		geometry_msgs::Pose baseTF;			// Transform from vehicle origin to manipulator base
+
 		mobile_manipulator::Vehicle vehicle;		// Vehicle object
+
 		SerialLink arm;					// Serial-Link object
 
 		// Constructor(s)
@@ -16,8 +19,8 @@ class MobileManipulator
 		MobileManipulator(int Hz);
 
 		// Get Functions
-		sensor_msgs::JointState rmrc(const geometry_msgs::Pose &pos, 
-					     const geometry_msgs::Twist &vel);
+		mobile_manipulator::Control rmrc(const geometry_msgs::Pose &pos, 
+					     	 geometry_msgs::Twist &vel);
 
 
 		// Set Functions
@@ -31,9 +34,17 @@ class MobileManipulator
 		// Properties
 		Eigen::MatrixXd A;				// B*T
 		Eigen::Matrix<double,6,6> B;			// Base velocity mapping to end-effector
+		Eigen::MatrixXd J;
 		Eigen::MatrixXd T;				// Thruster conversion matrix
+		Eigen::MatrixXd M1, M3;				// For kinematics calcs
+		Eigen::Matrix<double,6,6> M2;
 
+		Eigen::MatrixXd Wq;				// Joint weighting matrix 
+		Eigen::MatrixXd Wu;				// Thruster weighting matrix
+
+		int m;						// No. of thrusters
 		
+		mobile_manipulator::Control control_msg;	// Control message to be returned
 
 		// Get Functions
 
@@ -52,16 +63,28 @@ MobileManipulator::MobileManipulator()
 
 MobileManipulator::MobileManipulator(int Hz)
 {
-	const int n = this->vehicle.thruster.size();		// No. of thrusters in this model
+	// Step 1: Create the serial_link object
+	  /*** Code goes here ***/
 
+	// Step 2: Resize any dynamic memory arrays
 
-	this->A.resize(6,n);
-	this->B.setIdentity();
-	this->T.resize(6,n);
-	
+	this->m = this->vehicle.thruster.size();		// No. of thrusters in this model
 
-	// Compute thruster transform matrix
-	for(int i = 0; i < n; i++)
+	this->control_msg.jointControl.velocity.resize(this->arm.n);
+	this->control_msg.thrusterControl.resize(this->m);
+
+	this->A.resize(6,this->m);				// A = B*T
+	this->B.setIdentity();					// Maps vehicle velocity to end-effector velocity
+	this->T.resize(6,this->m);				// Maps thruster control to body velocity
+
+	this->M1.resize(this->m + this->arm.n,6);		// For kinematics calcs
+	this->M3.resize(this->m + this->arm.n,6);		// M3 = M1*M2
+
+	this->Wq = 10*Eigen::MatrixXd::Identity(this->arm.n, this->arm.n); // Joint weighting matrix
+	this->Wu = Eigen::MatrixXd::Identity(this->m, this->m);		   // Thruster weighting matrix
+
+	// Step 3: Compute thruster transform matrix
+	for(int i = 0; i < this->m; i++)
 	{
 		T(0,i) = vehicle.thruster[i].axis.x;
 		T(1,i) = vehicle.thruster[i].axis.y;
@@ -83,10 +106,10 @@ void MobileManipulator::updateState(const geometry_msgs::PoseStamped &input,
 				    const geometry_msgs::Accel &accel,
 				    const sensor_msgs::JointState &jointState)
 {
-	this->vehicle.tf = input;
-	this->vehicle.vel = vel;
-	this->vehicle.accel = accel;
-	this->arm.updateState(jointState,multiplyPose(input.pose,this->baseTF));
+	this->vehicle.tf = input;							// Assign pose
+	this->vehicle.vel = vel;							// Assign velocity
+	this->vehicle.accel = accel;							// Assign acceleration
+	this->arm.updateState(jointState,multiplyPose(input.pose,this->baseTF));	
 
 	// Update skew-symmetric component of base velocity mapping
 	double x = this->arm.FK.poses[this->arm.n].position.x - this->vehicle.tf.pose.position.x;
@@ -98,40 +121,70 @@ void MobileManipulator::updateState(const geometry_msgs::PoseStamped &input,
 	this->B(1,5) = x;
 	this->B(2,3) = y;
 	this->B(2,4) = -x;
+
+	this->A = this->B*this->T;		// Converts thruster control to end-effector velocity
 	
 }
 
-sensor_msgs::JointState MobileManipulator::rmrc(const geometry_msgs::Pose &pos,
-						const geometry_msgs::Twist &vel)
+mobile_manipulator::Control MobileManipulator::rmrc(const geometry_msgs::Pose &pos,
+						    geometry_msgs::Twist &vel)
 {
+	// Update the joint weight based on proximity to joint limits
+	for(int i = 0; i < this->arm.n; i++)
+	{
+		this->Wq(i,i) = getJointWeight(this->arm.jointState.position[i],
+					       this->arm.jointState.velocity[i],
+					       this->arm.serial.joint[i]);
+	}
 
-	
-/*geometry_msgs::Twist relative;					// Relative velocity to be passed to the manipulator
+	// Hopefully these assignments make calcs a little faster
+	Eigen::MatrixXd invWqJt = Wq.inverse()*this->arm.J.transpose();
+	Eigen::MatrixXd invWuAt = Wu.inverse()*this->A.transpose();	
 
-	// End-effector position - vehicle position
-	r.x = this->arm.FK[this->arm.n].position.x - this->vehicle.tf.pose.position.x;
-	r.y = this->arm.FK[this->arm.n].position.y - this->vehicle.tf.pose.position.y;
-	r.z = this->arm.FK[this->arm.n].position.z - this->vehicle.tf.pose.position.z;
+	// First component of weighted pseudoinverse
+	this->M1.block(0,0,this->arm.n,6) = invWqJt;
+	this->M1.block(this->arm.n,0,this->m,6) = invWuAt;
+
+	// Second component of weighted pseudoinverse
+	double lambda = this->arm.getDamping(this->arm.J);		// Apply DLS if arm is near-singular	
+	this->M2 = this->arm.J*invWqJt + this->A*invWuAt + lambda*Eigen::MatrixXd::Identity(6,6);
+				
+
+	this->M3 = M1*M2.inverse();					// This is the total pseudoinverse
 
 
-	// Relative linear velocity
-	relative.linear.x = vel.linear.x - this->vehicle.vel.linear.x
-					 + this->vehicle.vel.angluar.z*r.y
-					 - this->vehicle.vel.angular.y*r.x;
+	// Compute pose error and append feedback to velocity control
+	geometry_msgs::Pose error = getPoseError(pos, this->arm.FK.poses[this->arm.n]);
+	vel.linear.x += 0.9*error.position.x;
+	vel.linear.y += 0.9*error.position.y;
+	vel.linear.z += 0.9*error.position.z;
+	vel.angular.x += 0.9*error.orientation.x;
+	vel.angular.y += 0.9*error.orientation.y;
+	vel.angular.z += 0.9*error.orientation.z;
 
-	relative.linear.y = vel.linear.y - this->vehicle.vel.linear.y
-					 + this->vehicle.vel.angular.x*r.z
-					 - this->vehicle.vel.angular.z*r.x;
+	// Required joint control
+	for(int i = 0; i < this->arm.n; i++)
+	{
+		control_msg.jointControl.velocity[i] = M3(i,0)*vel.linear.x
+						     + M3(i,1)*vel.linear.y
+						     + M3(i,2)*vel.linear.z
+						     + M3(i,3)*vel.angular.x
+						     + M3(i,4)*vel.angular.y
+						     + M3(i,5)*vel.angular.z;
+	}
 
-	relative.linear.z = vel.linear.z - this->vehicle.vel.linear.z
-					 + this->vehicle.vel.angular.y*r.x
-					 - this->vehicle.vel.angular.x*r.y;
+	// Required thruster control
+	for(int i = this->arm.n; i < this->m + this->arm.n; i++)
+	{
+		control_msg.jointControl.velocity[i] = M3(i,0)*vel.linear.x
+						     + M3(i,1)*vel.linear.y
+						     + M3(i,2)*vel.linear.z
+						     + M3(i,3)*vel.angular.x
+						     + M3(i,4)*vel.angular.y
+						     + M3(i,5)*vel.angular.z;
+	}
 
-	// Relative angular velocity
-	relative.angular.x = vel.angular.x - this->vehicle.vel.angular.x;
-	relative.angular.y = vel.angular.y - this->vehicle.vel.angular.y;
-	relative.angular.z = vel.angular.z - this->vehicle.vel.angular.z;
-*/
+	return control_msg;
 }
 
 
